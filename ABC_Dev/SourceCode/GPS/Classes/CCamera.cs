58baecb91da2916082c5995a7e82ef54ef5768be
Nick Bennett <bennett.a.nicholas@gmail.com>
2020-06-08 08:48:56 -0500

    Added documentation for CCamera.cs

--- ABC_Dev/SourceCode/GPS/Classes/CCamera.cs
+++ ABC_Dev/SourceCode/GPS/Classes/CCamera.cs
@@ -23,15 +23,16 @@ namespace ABC
 
         public bool camFollowing;
 
-        //private double camDelta = 0;
+        // private double camDelta = 0;
 
         public CCamera()
         {
-            //get the pitch of camera from settings
+            // get the pitch of camera from settings
             camPitch = Properties.Settings.Default.setCam_pitch;
             camPosZ = 0.0;
             camFollowing = true;
         }
+        //please 
 
         /// <summary>
         /// This will set the position for the world camera
@@ -41,7 +42,9 @@ namespace ABC
         /// <param name="fixedHeading">The heading position for the camera</param>
         public void SetWorldCam(double fixPosX, double fixPosY, double fixedHeading)
         {
-            //initializing variables
+            // initializing variables
+
+            // offset for 3d mode (non "north lock" mode)
             const double fixedOffset = 0.02;
             camPosX = fixPosX;
             camPosY = fixPosY;
@@ -50,16 +53,16 @@ namespace ABC
             var sinHeading = Math.Sin(glm.toRadians(fixHeading));
             var cosHeading = Math.Cos(glm.toRadians(fixHeading));
 
-            //back the camera up
+            // back the camera up
             GL.Translate(0.0, 0.0, camSetDistance * 0.5);
-            //rotate the camera down to look at fix
+            // rotate the camera down to look at fix
             GL.Rotate(camPitch, 1.0, 0.0, 0.0);
 
-            //following game style or N fixed cam
+            // Camera mode is not locked in north facing position
             if (camFollowing)
             {
-                GL.Rotate(camYaw, 0.0, 0.0, 1.0);
-
+                GL.Rotate(camYaw*2, 0.0, 0.0, 1.0);
+                // Camera mode that is 3D
                 if (camPitch > -45)
                 {
                     offset = (45.0 + camPitch) / 45.0;
@@ -71,15 +74,16 @@ namespace ABC
                         -camPosY + (offset * camSetDistance * cosHeading),
                         -camPosZ);
                 }
+                // Camera mode that is 3D, but does not lock to the north
                 else
                 {
-                    //fixed offset of .02
                     GL.Translate(
                         -camPosX + (fixedOffset * camSetDistance * sinHeading),
                         -camPosY + (fixedOffset * camSetDistance * cosHeading),
                         -camPosZ);
                 }
             }
+            // Camera mode is locked in north facing position
             else
             {
                 GL.Translate(-camPosX, -camPosY, -camPosZ);
